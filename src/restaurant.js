a0c342a7f92cc76dcdbed93eb1ee5aa6c6b94dcf
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-06-21 11:44:54 -0300

    Initial commit

---
+++ src/restaurant.js
@@ -0,0 +1,6 @@
+/* eslint-disable max-len */
+// Siga as orientações do README!
+
+const createMenu = () => {};
+
+module.exports = createMenu;
---
+++ tests/restaurant.spec.js
@@ -0,0 +1,8 @@
+const createMenu = require('../src/restaurant');
+ 
+describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
+  it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
+    fail('Teste vazio!');
+    // Escreva todos os testes aqui.
+  });
+});
