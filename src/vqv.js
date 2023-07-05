a98c2afea7307371e622347f84c2fab41df7b44c
henriqueAvner <avnerhdpb@gmail.com>
2023-07-05 18:00:32 -0300

    fixed: Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -22,7 +22,10 @@ const vqv = (name, age) => {
   if (name === undefined || age === undefined) {
     return undefined;
   }
-  return `Oi, meu nome é ${name}!\nTenho ${age} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
-
+console.log(vqv('Tunico', 29));
 module.exports = vqv;
