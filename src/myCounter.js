68bd13a6ccfde8068167cb966c63e664cb46c9f6
henriqueAvner <avnerhdpb@gmail.com>
2023-07-05 21:05:58 -0300

    Requisito 8 do projeto

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,11 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
